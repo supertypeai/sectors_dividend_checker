58e034256bbd260d4a51984efd87b28629283aa6
Timotius Marselo <timotiusmarselo@gmail.com>
2024-01-09 20:16:32 +0700

    use pd timestamp instead of pytz

--- dividend_checker.py
+++ dividend_checker.py
@@ -11,11 +11,11 @@ load_dotenv()
 
 
 class DividendChecker:
-    def __init__(self, supabase_client, last_n_day="7"):
+    def __init__(self, supabase_client, last_n_day=7):
         self.url = "https://sahamidx.com/?view=Stock.Cash.Dividend&path=Stock&field_sort=rg_ng_ex_date&sort_by=DESC&page=1"
         self.supabase_client = supabase_client
-        self.start_date = (datetime.today() - timedelta(days=last_n_day - 1)).strftime("%Y-%m-%d")
-        self.end_date = datetime.today().strftime("%Y-%m-%d")
+        self.start_date = (pd.Timestamp.now("Asia/Bangkok") - pd.Timedelta(days=last_n_day - 1)).strftime("%Y-%m-%d")
+        self.end_date = pd.Timestamp.now("Asia/Bangkok").strftime("%Y-%m-%d")
         self.retrieved_records = []
 
     def get_dividend_records(self):
@@ -26,19 +26,20 @@ class DividendChecker:
         soup = BeautifulSoup(response.text, "lxml")
         table = soup.find("table", {"class": "tbl_border_gray"})
         rows = table.find_all("tr", recursive=False)[1:]
-        
+
         for row in rows:
             if len(row.find_all("td")) > 2:
                 values = row.find_all("td")
-                date = datetime.strptime(
-                    values[6].text.strip(), "%d-%b-%Y"
-                ).strftime("%Y-%m-%d")
+                date = datetime.strptime(values[6].text.strip(), "%d-%b-%Y").strftime(
+                    "%Y-%m-%d"
+                )
                 if self.start_date <= date <= self.end_date:
                     dividend = float(values[3].text.strip().replace(",", ""))
                     data_dict = {
                         "symbol": values[1].find("a").text.strip() + ".JK",
                         "date": date,
                         "dividend": round(dividend, 5),
+                        "updated_on": pd.Timestamp.now(tz="GMT").strftime("%Y-%m-%d %H:%M:%S"),
                     }
                     self.retrieved_records.append(data_dict)
 
@@ -48,8 +49,12 @@ class DividendChecker:
             raise SystemExit(0)
 
         try:
-            self.supabase_client.table("idx_dividend").upsert(self.retrieved_records).execute()
-            print(f"Successfully upserted {len(self.retrieved_records)} data to database")
+            self.supabase_client.table("idx_dividend").upsert(
+                self.retrieved_records
+            ).execute()
+            print(
+                f"Successfully upserted {len(self.retrieved_records)} data to database"
+            )
         except Exception as e:
             raise Exception(f"Error upserting to database: {e}")
 
