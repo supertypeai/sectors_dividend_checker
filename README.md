481cbd3535bbe3647d5dff3ee1598eb1a49272d3
Genvictus <johannstarkmk42@gmail.com>
2024-12-19 19:37:54 +0700

    add: IDX upcoming dividend data scraper

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Scripts for Dividend Checker
 
-This repository contains automated scripts in `dividend_checker.py` that are automatically executed by GitHub Actions to scrape data from specified websites. There are some records that scrapped in csv format.
+This repository contains automated scripts in `dividend_checker.py` that are automatically executed by GitHub Actions to
+scrape data from specified websites. There are some records that scrapped in csv format.
+
+This repository also contains scripts to scrape upcoming IDX dividend data
+from [investing.com](https://www.investing.com/dividends-calendar)
+at [`future_dividend_checker.py`](future_dividend_checker.py)
---
+++ future_dividend_checker.py
@@ -0,0 +1,97 @@
+import os
+import requests
+
+from datetime import datetime, timedelta, timezone
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup, Tag
+from supabase import create_client
+
+_DEFAULT_TIMEFRAME = 14  # days
+_LOCAL_TIMEZONE = 'Asia/Bangkok'
+
+load_dotenv()
+
+_SUPABASE_URL = os.getenv("SUPABASE_URL")
+_SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+_supabase_client = create_client(_SUPABASE_URL, _SUPABASE_KEY)
+
+_UPCOMING_DIVIDEND_URL = "https://www.investing.com/dividends-calendar/Service/getCalendarFilteredData"
+_UPCOMING_DIVIDEND_HEADERS = {
+    "Host": "www.investing.com",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0",
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Origin": "https://www.investing.com",
+    "Referer": "https://www.investing.com/dividends-calendar/",
+    "X-Requested-With": "XMLHttpRequest",
+}
+# _UPCOMING_DIVIDEND_BODY = {
+#     "&country[]": 48,
+#     "currentTab": "thisWeek",
+#     "limit_from": 0,
+# }
+_UPCOMING_DIVIDEND_BODY = r"&country%5B%5D=48&dateFrom={start_date}&dateTo={end_date}&submitFilters=1&limit_from=0"
+
+
+def parse_en_us_datetime(date_string: str):
+    return datetime.strptime(date_string, "%b %d, %Y")
+
+
+def get_upcoming_dividend_records(timeframe: int = _DEFAULT_TIMEFRAME):
+    start_date = datetime.now(tz=timezone.utc).date()
+    end_date = start_date + timedelta(days=timeframe)
+
+    retries = 1
+    while retries > 0:
+        try:
+            response = requests.post(
+                _UPCOMING_DIVIDEND_URL,
+                headers=_UPCOMING_DIVIDEND_HEADERS,
+                json=_UPCOMING_DIVIDEND_BODY.format(start_date=start_date.isoformat(), end_date=end_date.isoformat()),
+            )
+            response.raise_for_status()
+
+            json_response = response.json()
+            table = json_response["data"]
+
+            def exclude_tabledivider_tag(tag: Tag):
+                # filter out date divider entries
+                return tag.name == "tr" and not tag.has_attr("tablesorterdivider")
+
+            soup = BeautifulSoup(table, features="lxml")
+            rows = soup.find_all(exclude_tabledivider_tag)
+
+            parsed_data = []
+            for row in rows:
+                cols = row.find_all_next("td")
+
+                # Parse the data
+                raw_symbol = cols[1].find_next("a").getText()
+                ex_date = parse_en_us_datetime(cols[2].getText())
+                payment_date = parse_en_us_datetime(cols[5].getText())
+                dividend_amount = cols[3].getText()
+
+                # Convert into dictionary for Supabase client format
+                data_dict = {
+                    "symbol": f"{raw_symbol}.JK",
+                    "ex_date": ex_date.isoformat(),
+                    "payment_date": payment_date.isoformat(),
+                    "dividend_amount": dividend_amount,
+                    "updated_on": datetime.now(tz=timezone.utc).isoformat()
+                }
+
+                # Append the data into list
+                parsed_data.append(data_dict)
+
+            return parsed_data
+
+        except requests.HTTPError as e:
+            retries -= 1
+            print(f"failed to fetch data from {_UPCOMING_DIVIDEND_URL}: {e}")
+
+
+if __name__ == "__main__":
+    upcoming_dividend_data = get_upcoming_dividend_records()
+    _supabase_client.table("idx_upcoming_dividend").upsert(upcoming_dividend_data, ignore_duplicates=False).execute()
