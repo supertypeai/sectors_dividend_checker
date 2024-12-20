1f255ec0b642433f289169767621e37e3e329805
Genvictus <johannstarkmk42@gmail.com>
2024-12-20 18:18:09 +0700

    rfc: run python code formatting

--- dividend_checker.py
+++ dividend_checker.py
@@ -7,102 +7,103 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from supabase import create_client, Client
 import time
-from datetime import date 
+from datetime import date
 import yfinance as yf
 import numpy as np
 
 load_dotenv()
 
+
 def check_start_year():
-   today_date = date.today()
-   # Check if it is the first week of the first month in the year
-   return today_date.month == 1 and today_date.day <= 7
+    today_date = date.today()
+    # Check if it is the first week of the first month in the year
+    return today_date.month == 1 and today_date.day <= 7
 
 
 class DividendChecker:
-    def __init__(self, supabase_client : Client, last_n_day=7):
+    def __init__(self, supabase_client: Client, last_n_day=7):
         self.url = "https://sahamidx.com/?view=Stock.Cash.Dividend&path=Stock&field_sort=rg_ng_ex_date&sort_by=DESC&page=1"
         self.supabase_client = supabase_client
         self.start_date = (pd.Timestamp.now("Asia/Bangkok") - pd.Timedelta(days=last_n_day - 1)).strftime("%Y-%m-%d")
         self.end_date = pd.Timestamp.now("Asia/Bangkok").strftime("%Y-%m-%d")
         self.retrieved_records = []
-        self.allowed_symbols = [k['symbol'][:4] for k in self.supabase_client.from_("idx_company_profile").select("symbol").execute().data]
+        self.allowed_symbols = [k['symbol'][:4] for k in
+                                self.supabase_client.from_("idx_company_profile").select("symbol").execute().data]
 
     def get_dividend_records(self):
         attempt = 1
         max_attempt = 10
-        while(attempt <= max_attempt):
-          try:
-            response = requests.get(self.url)
-            if response.status_code != 200:
-                raise Exception("Error retrieving data from SahamIDX")
+        while (attempt <= max_attempt):
+            try:
+                response = requests.get(self.url)
+                if response.status_code != 200:
+                    raise Exception("Error retrieving data from SahamIDX")
 
-            soup = BeautifulSoup(response.text, "lxml")
-            table = soup.find("table", {"class": "tbl_border_gray"})
-            rows = table.find_all("tr", recursive=False)[1:]
-            # parameter to iterate the rows
-            num_of_rows = len(rows)
-            counter = num_of_rows - 1
-            # stack to store the rows
-            stack = []
-            while(True):
-                row = rows[counter]
-                # push into stack
-                stack.append(row)
-                if len(row.find_all("td")) > 2:
-                    values : str = row.find_all("td")[0].text.strip()
-                    # can value typecast to int?
-                    try:
-                        int(values)
-                        
-                        #invert the stack
-                        stack = stack[::-1]
-                        # get first element
-                        first_element = stack[0]
-                        # get as td
-                        first_element_td = first_element.find_all("td")
-                        # get symbol
-                        symbol = first_element_td[1].find("a").text.strip()
-                            
-                        # get dividend original
-                        dividend_original = float(first_element_td[3].text.strip())
-                        # get date
-                        date = datetime.strptime(first_element_td[6].text.strip(), "%d-%b-%Y").strftime(
-                            "%Y-%m-%d")
-                        
-                        # Adjust the symbol
-                        adjusted_symbol = symbol + ".JK"
+                soup = BeautifulSoup(response.text, "lxml")
+                table = soup.find("table", {"class": "tbl_border_gray"})
+                rows = table.find_all("tr", recursive=False)[1:]
+                # parameter to iterate the rows
+                num_of_rows = len(rows)
+                counter = num_of_rows - 1
+                # stack to store the rows
+                stack = []
+                while (True):
+                    row = rows[counter]
+                    # push into stack
+                    stack.append(row)
+                    if len(row.find_all("td")) > 2:
+                        values: str = row.find_all("td")[0].text.strip()
+                        # can value typecast to int?
+                        try:
+                            int(values)
 
+                            # invert the stack
+                            stack = stack[::-1]
+                            # get first element
+                            first_element = stack[0]
+                            # get as td
+                            first_element_td = first_element.find_all("td")
+                            # get symbol
+                            symbol = first_element_td[1].find("a").text.strip()
 
-                        if (symbol in self.allowed_symbols) and(self.start_date <= date <= self.end_date):
-                            data_dict = {
-                                "symbol": adjusted_symbol,
-                                "date": date,
-                                "dividend_original": dividend_original,
-                                "dividend" : dividend_original,
-                                "updated_on": pd.Timestamp.now(tz="GMT").strftime("%Y-%m-%d %H:%M:%S"),
-                            }
-                            print(f'[FETCHING] {data_dict}')
-                            self.retrieved_records.append(data_dict)
-                    except:
-                        pass
-                    #clear the stack
-                    stack = []
-                # reduce counter
-                counter -= 1
-                if(counter < 0):
-                    break
-            
-            # Break the attempt while if success
-            break
-          except Exception as e:
-            if (attempt == max_attempt):
-              print(f"\t[ATTEMPTS FAILED] Failed after {max_attempt} attempts. | {e}")
+                            # get dividend original
+                            dividend_original = float(first_element_td[3].text.strip())
+                            # get date
+                            date = datetime.strptime(first_element_td[6].text.strip(), "%d-%b-%Y").strftime(
+                                "%Y-%m-%d")
 
-            else:
-              print(f"\t[FAILED] Failed after {attempt} attempt(s). Retrying after 2 seconds... | {e}")
-              time.sleep(2)
-            attempt += 1
+                            # Adjust the symbol
+                            adjusted_symbol = symbol + ".JK"
+
+                            if (symbol in self.allowed_symbols) and (self.start_date <= date <= self.end_date):
+                                data_dict = {
+                                    "symbol": adjusted_symbol,
+                                    "date": date,
+                                    "dividend_original": dividend_original,
+                                    "dividend": dividend_original,
+                                    "updated_on": pd.Timestamp.now(tz="GMT").strftime("%Y-%m-%d %H:%M:%S"),
+                                }
+                                print(f'[FETCHING] {data_dict}')
+                                self.retrieved_records.append(data_dict)
+                        except:
+                            pass
+                        # clear the stack
+                        stack = []
+                    # reduce counter
+                    counter -= 1
+                    if (counter < 0):
+                        break
+
+                # Break the attempt while if success
+                break
+            except Exception as e:
+                if (attempt == max_attempt):
+                    print(f"\t[ATTEMPTS FAILED] Failed after {max_attempt} attempts. | {e}")
+
+                else:
+                    print(f"\t[FAILED] Failed after {attempt} attempt(s). Retrying after 2 seconds... | {e}")
+                    time.sleep(2)
+                attempt += 1
 
     def upsert_to_db(self):
         if not self.retrieved_records:
@@ -118,89 +119,91 @@ class DividendChecker:
             )
         except Exception as e:
             raise Exception(f"Error upserting to database: {e}")
-        
-    def upsert_yield_in_db(self):
-      database_data = supabase_client.from_("idx_dividend").select("*").execute().data
-      db_df = pd.DataFrame(database_data)
-      
-      count = 0
-      for index, row in db_df.iterrows():
-          if (row['yield'] is None):
-            ticker = row['symbol']
-            dividend_date = row['date']
-            dividend_year = int(dividend_date.split("-")[0])
-            current_year = datetime.now().year
 
-            if (dividend_year < current_year):
-              # If starting from that year
-              start_date = f"{dividend_year}-01-01"
-              end_date = f"{dividend_year}-12-31"
-              count += 1
-              
-              stock = yf.Ticker(ticker).history(start=start_date, end=end_date, auto_adjust=False) 
-              stock = stock[["Close"]] # Get only the Close data
-              mean_val = stock.mean().values[0]
-              yield_val = row['dividend']/mean_val
-              db_df.at[index, 'yield'] = yield_val
-              db_df.at[index, 'updated_on'] = pd.Timestamp.now(tz="GMT").strftime("%Y-%m-%d %H:%M:%S")
-              print(f"[UPDATING YIELD] {ticker} {dividend_date} yield : {yield_val}")
-      
-      db_df = db_df.replace({np.nan : None})
-      # Upsert to db the result
-      try:
+    def upsert_yield_in_db(self):
+        database_data = supabase_client.from_("idx_dividend").select("*").execute().data
+        db_df = pd.DataFrame(database_data)
+
+        count = 0
+        for index, row in db_df.iterrows():
+            if (row['yield'] is None):
+                ticker = row['symbol']
+                dividend_date = row['date']
+                dividend_year = int(dividend_date.split("-")[0])
+                current_year = datetime.now().year
+
+                if (dividend_year < current_year):
+                    # If starting from that year
+                    start_date = f"{dividend_year}-01-01"
+                    end_date = f"{dividend_year}-12-31"
+                    count += 1
+
+                    stock = yf.Ticker(ticker).history(start=start_date, end=end_date, auto_adjust=False)
+                    stock = stock[["Close"]]  # Get only the Close data
+                    mean_val = stock.mean().values[0]
+                    yield_val = row['dividend'] / mean_val
+                    db_df.at[index, 'yield'] = yield_val
+                    db_df.at[index, 'updated_on'] = pd.Timestamp.now(tz="GMT").strftime("%Y-%m-%d %H:%M:%S")
+                    print(f"[UPDATING YIELD] {ticker} {dividend_date} yield : {yield_val}")
+
+        db_df = db_df.replace({np.nan: None})
+        # Upsert to db the result
+        try:
             self.supabase_client.table("idx_dividend").upsert(
                 db_df.to_dict(orient="records")
             ).execute()
             print(
                 f"Successfully updated {count} yield data in database"
             )
-      except Exception as e:
-            raise Exception(f"Error upserting to database: {e}")
-      
-    def check_stock_split(self):
-      stock_split_data = self.supabase_client.from_("idx_stock_split").select("symbol, date, split_ratio").execute().data
-      stock_split_df = pd.DataFrame(stock_split_data)
-      database_data = supabase_client.from_("idx_dividend").select("*").execute().data
-      database_df = pd.DataFrame(database_data)
-      count = 0
-      found = False
-      for _, ss_row in stock_split_df.iterrows():
-          current_time = datetime.now()
-          ss_time = datetime.strptime(ss_row['date'], "%Y-%m-%d")
-          diff = current_time - ss_time
-          # Check if there is any stock split happening recently
-          if (ss_time < current_time and diff.days <= 7):
-              found = True
-              symbol = ss_row['symbol']
-              ratio = ss_row['split_ratio']
-              # Get df that is the symbol
-              print(f"[FOUND NEW STOCK SPLIT] {symbol} {ss_row['date']}")
-              symbol_df = database_df[database_df['symbol'] == symbol]
-
-              for s_idx, s_row in symbol_df.iterrows():
-                dividend_time = datetime.strptime(s_row['date'], "%Y-%m-%d")
-                # Update dividend data 
-                if (dividend_time < ss_time):
-                  old_dividend = s_row['dividend']
-                  new_dividend = old_dividend / ratio
-                  database_df.at[s_idx, "dividend"] = new_dividend
-                  database_df.at[s_idx, "updated_on"] = pd.Timestamp.now(tz="GMT").strftime("%Y-%m-%d %H:%M:%S")
-                  count += 1
-                  print(f"[UPDATING DIVIDEND] {symbol} {s_row['date']} {old_dividend} -> {new_dividend} ratio {ratio}")
-      
-      database_df = database_df.replace({np.nan : None})
-      # Upsert to db the result
-      if (found):
-        try:
-              self.supabase_client.table("idx_dividend").upsert(
-                  database_df.to_dict(orient="records")
-              ).execute()
-              print(
-                  f"Successfully updated {count} dividend data in database"
-              )
         except Exception as e:
-              raise Exception(f"Error upserting to database: {e}")
-       
+            raise Exception(f"Error upserting to database: {e}")
+
+    def check_stock_split(self):
+        stock_split_data = self.supabase_client.from_("idx_stock_split").select(
+            "symbol, date, split_ratio").execute().data
+        stock_split_df = pd.DataFrame(stock_split_data)
+        database_data = supabase_client.from_("idx_dividend").select("*").execute().data
+        database_df = pd.DataFrame(database_data)
+        count = 0
+        found = False
+        for _, ss_row in stock_split_df.iterrows():
+            current_time = datetime.now()
+            ss_time = datetime.strptime(ss_row['date'], "%Y-%m-%d")
+            diff = current_time - ss_time
+            # Check if there is any stock split happening recently
+            if (ss_time < current_time and diff.days <= 7):
+                found = True
+                symbol = ss_row['symbol']
+                ratio = ss_row['split_ratio']
+                # Get df that is the symbol
+                print(f"[FOUND NEW STOCK SPLIT] {symbol} {ss_row['date']}")
+                symbol_df = database_df[database_df['symbol'] == symbol]
+
+                for s_idx, s_row in symbol_df.iterrows():
+                    dividend_time = datetime.strptime(s_row['date'], "%Y-%m-%d")
+                    # Update dividend data
+                    if (dividend_time < ss_time):
+                        old_dividend = s_row['dividend']
+                        new_dividend = old_dividend / ratio
+                        database_df.at[s_idx, "dividend"] = new_dividend
+                        database_df.at[s_idx, "updated_on"] = pd.Timestamp.now(tz="GMT").strftime("%Y-%m-%d %H:%M:%S")
+                        count += 1
+                        print(
+                            f"[UPDATING DIVIDEND] {symbol} {s_row['date']} {old_dividend} -> {new_dividend} ratio {ratio}")
+
+        database_df = database_df.replace({np.nan: None})
+        # Upsert to db the result
+        if (found):
+            try:
+                self.supabase_client.table("idx_dividend").upsert(
+                    database_df.to_dict(orient="records")
+                ).execute()
+                print(
+                    f"Successfully updated {count} dividend data in database"
+                )
+            except Exception as e:
+                raise Exception(f"Error upserting to database: {e}")
+
 
 if __name__ == "__main__":
     url, key = os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY")
@@ -210,5 +213,5 @@ if __name__ == "__main__":
     stock_split_checker.get_dividend_records()
     stock_split_checker.upsert_to_db()
     stock_split_checker.check_stock_split()
-    if (check_start_year()):
-      stock_split_checker.upsert_yield_in_db()
+    if check_start_year():
+        stock_split_checker.upsert_yield_in_db()
